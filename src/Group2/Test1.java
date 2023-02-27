0b6c723dc0d76a5e2ebf649ca448ba6e757b05aa
Ali Riza IMRE <121388004+AliRizaIMRE@users.noreply.github.com>
2023-02-27 14:04:26 +0100

    Update Test1.java

--- src/Group2/Test1.java
+++ src/Group2/Test1.java
@@ -3,5 +3,6 @@ package Group2;
 public class Test1 {
     // Test 1 class ---> Githup Task
     // gitHup tan güncelleme yaptim!!! REMOTE!!!
+    // gitHup tan baglanip güncelleme yaptim!!! 27.02.2023 --> REMOTE
 
 }
